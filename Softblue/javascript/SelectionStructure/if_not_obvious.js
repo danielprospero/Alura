55cf38f2c9ba0ee4b82d2365962526855c81f28d
Daniel Próspero Ribeiro <danielprosperoribeiro@outlook.com.br>
2022-08-25 15:53:08 -0300

    Delete Softblue directory

--- Softblue/javascript/SelectionStructure/if_not_obvious.js
+++
@@ -1,9 +0,0 @@
-// -> False: 0, undefined, null, "", NaN
-let x = 10;
-console.log(x);
-
-if (x = 2) {
-    console.log("Bloco executado");
-}
-
-console.log("Fim do programa");
--- Softblue/javascript/Types/null_undefined.js
+++
@@ -1,9 +0,0 @@
-let x = 5;
-console.log(x);
-x = null;
-console.log(x);
-
-let y;
-console.log(y);
-y = undefined;
-console.log(y);
